b7d10363f6dfb66d52fc2adec78bba362de66928
Daniel Córdova A <danesc87@gmail.com>
2018-07-26 22:21:50 -0500

    some mods to make rest

--- app/views.py
+++ app/views.py
@@ -1,6 +1,33 @@
-from flask import render_template, request
+from flask import render_template, request, jsonify, abort
 from app import app, db
 from .models import BookType
+
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web',
+        'done': False
+    }
+]
+
+@app.route('/tasks')
+def get_tasks():
+    return jsonify({'tasks': tasks})
+
+@app.route('/tasks/<int:task_id>', methods=['GET'])
+def get_task_by_id(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    return jsonify({'task': task[0]})
 
 @app.route('/')
 @app.route('/index')
