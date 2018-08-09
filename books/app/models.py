e332fd8b8efb784d1e8e7d2aee230dbfd45bd212
Daniel Córdova A <danesc87@gmail.com>
2018-08-09 12:29:31 -0500

    Add Post books and get all books

--- books/app/models.py
+++ books/app/models.py
@@ -9,7 +9,8 @@ from app import db
 class BookType(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     type = db.Column(db.String(100), index=True, unique=True, nullable=False)
-    genres = db.relationship('BookGenre', backref='types', lazy='dynamic')
+    genres = db.relationship('BookGenre', backref='gender_type', lazy='dynamic')
+    book = db.relationship('Book', backref='type', lazy='dynamic')
 
     def json_dump(self):
         return dict(id=self.id, type=self.type)
@@ -21,7 +22,7 @@ class BookGenre(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     genre = db.Column(db.String(100), index=True, nullable=False)
     type = db.Column(db.String(100), db.ForeignKey('book_type.type'), nullable=False)
-    books = db.relationship('Books', backref='gender', lazy='dynamic')
+    book = db.relationship('Book', backref='gender', lazy='dynamic')
 
     def json_dump(self):
         return dict(id=self.id, genre=self.genre, type=self.type)
@@ -33,10 +34,10 @@ class Author(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(150), index=True, nullable=False)
     lastname = db.Column(db.String(150), index=True, nullable=False)
-    books_author_name = db.relationship('Books', backref='author_name', lazy='dynamic',
-                                        foreign_keys='Books.book_author_name')
-    books_author_lastname = db.relationship('Books', backref='author_lastname', lazy='dynamic',
-                                            foreign_keys='Books.book_author_lastname')
+    book_author_name = db.relationship('Book', backref='name', lazy='dynamic',
+                                        foreign_keys='Book.book_author_name')
+    book_author_lastname = db.relationship('Book', backref='lastname', lazy='dynamic',
+                                            foreign_keys='Book.book_author_lastname')
 
     def json_dump(self):
         return dict(id=self.id, name=self.name, lastname=self.lastname)
@@ -44,7 +45,7 @@ class Author(db.Model):
     def __repr__(self):
         return '<Author %r>' % (self.name)
 
-class Books(db.Model):
+class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     book_name = db.Column(db.String(300), nullable=False)
     book_type = db.Column(db.String(100), db.ForeignKey('book_type.type'), nullable=False)
@@ -53,6 +54,11 @@ class Books(db.Model):
     book_author_lastname = db.Column(db.String(150), db.ForeignKey('author.lastname'), nullable=False)
     status = db.Column(db.Integer)
     statusperson = db.Column(db.String(150))
+
+    def json_dump(self):
+        return dict(id=self.id, book_name=self.book_name, book_type=self.book_type,
+                    book_genre=self.book_genre, book_author_name=self.book_author_name,
+                    book_author_lastname=self.book_author_lastname)
 
     def __repr__(self):
         return '<Name %r>' % (self.book_name)
--- books/app/views/book_views.py
+++ books/app/views/book_views.py
@@ -3,3 +3,47 @@
 '''
  Author: Daniel Córdova A.
 '''
+
+from flask import abort, request
+from flask import jsonify
+
+from app import app, db, custom_messages
+from . import book_path
+
+from app.models import Book
+
+@app.route(book_path, methods=['POST'])
+def post_new_book():
+    if not request.json:
+        abort(400)
+
+    request_body = request.json
+    book_name = request_body.get('book_name')
+    book_type = request_body.get('book_type')
+    book_genre = request_body.get('book_genre')
+    book_author_name = request_body.get('book_author_name')
+    book_author_lastname = request_body.get('book_author_lastname')
+
+    [check_empty_fields(request_body.get(field)) for field in request_body]
+
+    new_book = Book(
+        book_name=book_name,
+        book_type=book_type,
+        book_genre=book_genre,
+        book_author_name=book_author_name,
+        book_author_lastname=book_author_lastname
+    )
+
+    db.session.add(new_book)
+    db.session.commit()
+
+    return custom_messages.succesfully_stored_on_db(book_name + ' Book'), 201
+
+@app.route(book_path, methods=['GET'])
+def get_all_books():
+    all_books = Book.query.all()
+    return jsonify([each_book.json_dump() for each_book in all_books])
+
+def check_empty_fields(field):
+    if field == "" or field == None:
+        abort(400)
