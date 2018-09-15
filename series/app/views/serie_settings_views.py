e77ce68d009539794de08e569c2ca8481b8d0902
Daniel Córdova A <danesc87@gmail.com>
2018-09-15 17:33:08 -0500

    Add get by name and delete serie type

--- series/app/views/serie_settings_views.py
+++ series/app/views/serie_settings_views.py
@@ -7,12 +7,13 @@
 from flask import abort, request
 from flask import jsonify
 from sqlalchemy import exc
-from app.models import SerieType
-from app.utilities import is_not_json_request, check_empty_values
-from app.custom_messages import succesfully_stored_on_db, succesfully_deleted_from_db
 
 from app import app, db
-from . import serie_path, serie_type_path, serie_genre_path
+from app import custom_messages
+from app.models import SerieType
+from app.utilities import exist_object_on_database
+from app.utilities import is_not_json_request, check_empty_values
+from . import serie_path, serie_type_path
 
 
 #############
@@ -32,11 +33,44 @@ def post_new_serie_type():
     except exc.IntegrityError:
         abort(400)
 
-    return succesfully_stored_on_db(
+    return custom_messages.succesfully_stored_on_db(
         serie_type + 'type'
     ), 201
+
 
 @app.route(serie_path + serie_type_path, methods=['GET'])
 def get_series_types():
     all_serie_types = SerieType.query.all()
     return jsonify([serie_type.json_dump() for serie_type in all_serie_types])
+
+
+@app.route(serie_path + serie_type_path + '/<string:serie_type_name>', methods=['GET'])
+def get_serie_type_by_name(serie_type_name):
+
+    check_empty_values(serie_type_name)
+    serie_type = SerieType.query.filter_by(
+        type=serie_type_name
+    ).first()
+    exist_object_on_database(serie_type)
+    return jsonify(
+        serie_type.json_dump()
+    )
+
+
+@app.route(serie_path + serie_type_path, methods=['DELETE'])
+def delete_serie_type_by_name():
+
+    is_not_json_request(request)
+    serie_type = request.json.get('type')
+    check_empty_values(serie_type)
+    deleted_serie_type = SerieType.query.filter_by(
+        type=serie_type
+    ).order_by(
+        SerieType.id.desc()
+    ).first()
+    exist_object_on_database(deleted_serie_type)
+    db.session.delete(deleted_serie_type)
+    db.session.commit()
+    return custom_messages.succesfully_deleted_from_db(
+        serie_type + " type"
+    )
