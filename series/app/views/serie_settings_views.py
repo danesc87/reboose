0c620e733b755cdc7004187d4a1d8bce58cae92b
Daniel Córdova A <danesc87@gmail.com>
2018-09-15 17:17:44 -0500

    Add Post for serie type

--- series/app/views/serie_settings_views.py
+++ series/app/views/serie_settings_views.py
@@ -7,6 +7,9 @@
 from flask import abort, request
 from flask import jsonify
 from sqlalchemy import exc
+from app.models import SerieType
+from app.utilities import is_not_json_request, check_empty_values
+from app.custom_messages import succesfully_stored_on_db, succesfully_deleted_from_db
 
 from app import app, db
 from . import serie_path, serie_type_path, serie_genre_path
@@ -18,5 +21,17 @@ from . import serie_path, serie_type_path, serie_genre_path
 
 @app.route(serie_path + serie_type_path, methods=['POST'])
 def post_new_serie_type():
-    print('Hola')
-    return jsonify({'message': 'It Worked!'})
+    is_not_json_request(request)
+    try:
+        serie_type = request.json.get('type')
+        check_empty_values(serie_type)
+
+        new_serie_type = SerieType(type=serie_type)
+        db.session.add(new_serie_type)
+        db.session.commit()
+    except exc.IntegrityError:
+        abort(400)
+
+    return succesfully_stored_on_db(
+        serie_type + 'type'
+    ), 201
