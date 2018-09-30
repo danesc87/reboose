6bbe49909bbc0029c59501c3270722fa08952b27
Daniel Córdova A <danesc87@gmail.com>
2018-09-29 23:46:05 -0500

    Add initial scaffold with Vue.js and add CORS to backends

--- books/app/__init__.py
+++ books/app/__init__.py
@@ -7,11 +7,13 @@
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
+CORS(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
