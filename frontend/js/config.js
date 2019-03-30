09182273117223dbd9c45d9aeafe88ebfe07d697
Daniel Córdova A <danesc87@gmail.com>
2019-03-30 16:42:56 -0500

    Some Js for Series Settings Scaffold

---
+++ frontend/js/config.js
@@ -0,0 +1,34 @@
+/*
+Author: Daniel Córdova A.
+*/
+/**
+ * This file has some configurations to make it easier the development of the frontend
+ */
+
+ //
+ const httpProtocol = 'http://'
+
+// MicroServices configs
+const bookMicroServiceConfig = {
+    ipAddress: '127.0.0.1',
+    port: '2050'
+};
+
+const seriesMicroServiceConfig = {
+    ipAddress: '127.0.0.1',
+    port: '2051'
+};
+
+const paths = {
+    bookPath: 'book',
+    seriesPath: 'series',
+    typePath: 'type',
+    genrePath: 'genre',
+    authorPath: 'author'
+};
+
+// Series Id's
+var seriesIds = {
+    seriesTypeId: '#series-settings',
+    newSerieTypeId: 'new-type'
+};
---
+++ frontend/js/series-settings.js
@@ -0,0 +1,50 @@
+/*
+Author: Daniel Córdova A.
+*/
+
+/**
+ * This file has requests for series settings includes SeriesTypes and SeriesGenres
+ */
+
+const seriesFullPath = httpProtocol.concat('/')
+  .concat(seriesMicroServiceConfig.ipAddress)
+  .concat(':')
+  .concat(seriesMicroServiceConfig.port)
+  .concat('/')
+  .concat(paths.seriesPath);
+
+const seriesTypePath = seriesFullPath
+  .concat('/')
+  .concat(paths.typePath);
+
+const seriesGenrePath = seriesFullPath
+  .concat('/')
+  .concat(paths.genrePath);
+
+var serieTypes = new Vue({
+  el: seriesIds.seriesTypeId,
+  data: {
+    types: [],
+    genres: []
+  },
+  methods: {
+    getSeriesTypes: function() {
+      axios.get(seriesTypePath)
+        .then(response => {this.types = response.data})
+        .catch(error => console.log(error))
+    },
+    postSeriesTypes: function(e){
+      var newType = document.getElementById(seriesIds.newSerieTypeId);
+      var inputJson = {
+        type_name: newType.value
+      }
+
+      axios.post(seriesTypePath, inputJson)
+    },
+    getSeriesGenres: function() {
+      axios.get(seriesGenrePath)
+        .then(response => {this.genres = response.data})
+        .catch(error => console.log(error))
+    }
+  }
+});
